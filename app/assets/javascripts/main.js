5d2a7cd49a17d6871ee4d9a5ded7a3bd565c8bb9
alaughlin <adamlaughlin@outlook.com>
2014-10-14 20:18:19 -0400

    Major refactor of main.js.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,54 +1,75 @@
 (function() {
   var app = window.app = {};
+  var app.map;
+  var app.pairs = {};
 
   app.initialize = function() {
     navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+
       var mapOptions = {
         center: {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
+          lat: lat,
+          lng: lng
         },
         zoom: 12
       };
 
-      var map = new google.maps.Map($('#map-canvas')[0], mapOptions);
-      var url = 'http://localhost:3000/api/events?lat=' + position.coords.latitude + ' &long=' + position.coords.longitude
+      app.map = new google.maps.Map($('#map-canvas')[0], mapOptions);
 
-      $.ajax({
-        url: url,
-        success: function(data) {
-          _.each(data.events.event, function(event) {
-            var lat = event.venue.location['geo:point']['geo:lat'];
-            var long = event.venue.location['geo:point']['geo:long'];
-            var latLong = new google.maps.LatLng(lat, long);
+      app.getEvents(lat, lng);
+    });
+  };
 
-            var title = event.title;
-            var venue = event.venue;
-            var li = $('<li>' + venue.name + '</li>');
-
-            $('#locations').append(li);
-
-            var info = new google.maps.InfoWindow({
-              content: title
-            });
-
-            var marker = new google.maps.Marker({
-              position: latLong,
-              map: map,
-              title: title,
-              animation: google.maps.Animation.DROP,
-            });
-
-            google.maps.event.addListener(marker, 'click', function() {
-              info.open(map, marker);
-            });
-
-            google.maps.event.addListener(li, 'click', function() {
-              console.log(what);
-            });
-          });
+  app.getEvents = function(lat, lng) {
+    $.ajax({
+      url: 'http://localhost:3000/api/events',
+      data: {
+        lat: lat,
+        long: lng
+      },
+      success: function(data) {
+        _.each(data.events.event, function(event) {
+          app.placeMarker(event);
         }
-      });
+      }
+    });
+  };
+
+  app.placeMarker = function(event) {
+    var lat = event.venue.location['geo:point']['geo:lat'];
+    var lng = event.venue.location['geo:point']['geo:long'];
+    var latLong = new google.maps.LatLng(lat, lng);
+
+    var venue = event.venue;
+    var li = $('<li>' + venue.name + '</li>');
+    $('#locations').append(li);
+
+    var title = event.title;
+    var info = new google.maps.InfoWindow({
+      content: title
+    });
+
+    var marker = new google.maps.Marker({
+      position: latLong,
+      map: app.map,
+      title: title,
+      animation: google.maps.Animation.DROP,
+    });
+
+    app.pairs[li] = marker;
+
+    app.setUpListeners(marker, li);
+  };
+
+  app.setUpListeners = function(marker, li) {
+    google.maps.event.addListener(marker, 'click', function() {
+      info.open(app.map, marker);
+    });
+
+    google.maps.event.addListener(li, 'click', function() {
+      info.open(app.map, app.pairs[li]);
     });
   }
 })();
--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -7,7 +7,7 @@ class Api::EventsController < ApplicationController
     url = URI.parse(encoded_uri)
 
     req = Net::HTTP::Get.new(url.to_s)
-    res = Net::HTTP.start(url.host, url.port) { |http| http.request(req)}
+    res = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
 
     render json: res.body
   end
